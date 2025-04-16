b5ff4f5f45368c6952ec8d60a814a4127e38e3b2
killown <systemofdown@gmai.com>
2025-04-16 14:42:07 -0300

    Fix type handling for view

--- plugins/wm-actions/wm-actions.cpp
+++ plugins/wm-actions/wm-actions.cpp
@@ -425,7 +425,7 @@ class wayfire_wm_actions_t : public wf::plugin_interface_t,
     wf::json_t execute_for_view(const wf::json_t& params,
         std::function<void(wayfire_toplevel_view, bool)> view_op)
     {
-        uint64_t view_id = wf::ipc::json_get_uint64(params, "view-id");
+        auto view_id = wf::ipc::get_view_id(params);
         bool state = wf::ipc::json_get_bool(params, "state");
         wayfire_toplevel_view view = toplevel_cast(wf::ipc::find_view_by_id(view_id));
         if (!view)
