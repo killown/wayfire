9ad6a8e57d3ef0d185e1e71904c4393793bfcdbc
killown <systemofdown@gmai.com>
2025-05-04 01:11:01 -0300

    Apply Uncrustify formatting

--- plugins/ipc/ipc-helpers.hpp
+++ plugins/ipc/ipc-helpers.hpp
@@ -117,6 +117,7 @@ inline std::optional<int64_t> get_output_id(const wf::json_t& data)
     {
         output_id = wf::ipc::json_get_optional_int64(data, "output-id");
     }
+
     if (!output_id.has_value())
     {
         output_id = wf::ipc::json_get_optional_int64(data, "id");
