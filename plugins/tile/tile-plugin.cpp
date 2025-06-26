fcdb1ce08b8a8ed0501322337487aae0006d6b70
killown <systemofdown@gmai.com>
2025-06-26 13:51:08 -0300

    add fullscreen adjacent logic

--- plugins/tile/tile-plugin.cpp
+++ plugins/tile/tile-plugin.cpp
@@ -311,7 +311,7 @@ class tile_output_plugin_t : public wf::pointer_interaction_t, public wf::custom
     
             if (!adjacent)
                 return false;
-    
+   
             auto current_node = tile::view_node_t::get_node(view);
             bool was_maximized = current_node->show_maximized;
     
@@ -333,6 +333,13 @@ class tile_output_plugin_t : public wf::pointer_interaction_t, public wf::custom
 
             wf::get_core().seat->focus_view(adjacent->view);
             view_bring_to_front(adjacent->view);
+
+            /* This will lower the fullscreen status of the view */
+            bool was_fullscreen = view->pending_fullscreen();
+            if (was_fullscreen && keep_fullscreen_on_adjacent)
+            {
+                wf::get_core().default_wm->fullscreen_request(adjacent->view, output, true);
+            }
     
             return true;
         });
