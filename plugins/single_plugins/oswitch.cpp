2176ca690b3023f1af8a20c76e12e56499e6333c
killown <systemofdown@gmai.com>
2025-07-01 01:59:25 -0300

    oswitch add directions to output switching

--- plugins/single_plugins/oswitch.cpp
+++ plugins/single_plugins/oswitch.cpp
@@ -13,6 +13,11 @@ class wayfire_oswitch : public wf::plugin_interface_t
 {
     wf::wl_idle_call idle_switch_output;
 
+    wf::output_t *get_left_output()  { return get_output_in_direction(-1, 0); }
+    wf::output_t *get_right_output() { return get_output_in_direction(1, 0); }
+    wf::output_t *get_up_output()    { return get_output_in_direction(0, -1); }
+    wf::output_t *get_down_output()  { return get_output_in_direction(0, 1); }
+
     wf::output_t *get_output_relative(int step)
     {
         /* get the target output n steps after current output
@@ -33,8 +38,44 @@ class wayfire_oswitch : public wf::plugin_interface_t
         return os[target_index];
     }
 
+    wf::output_t *get_output_in_direction(int dir_x, int dir_y)
+    {
+        auto current_output = wf::get_core().seat->get_active_output();  
+        if (!current_output)
+            return nullptr;
+
+        auto current_geo = current_output->get_layout_geometry();
+    
+        wf::output_t *best_output = nullptr;
+        double best_score = -INFINITY;
+    
+        for (auto& output : wf::get_core().output_layout->get_outputs())
+        {
+            if (output == current_output)
+                continue;
+    
+            auto geo = output->get_layout_geometry();
+            double dx = (geo.x + geo.width / 2) - (current_geo.x + current_geo.width / 2);
+            double dy = (geo.y + geo.height / 2) - (current_geo.y + current_geo.height / 2);
+
+            double score = dx * dir_x + dy * dir_y;
+    
+            if (score > 0 && (best_output == nullptr || score < best_score))
+            {
+                best_output = output;
+                best_score = score;
+            }
+        }
+    
+        return best_output ? best_output : current_output;
+    }
+    
     void switch_to_output(wf::output_t *target_output)
     {
+        if (!target_output) {
+            LOGI("No output found in requested direction. Cannot switch.");
+            return;
+        }
         /* when we switch the output, the oswitch keybinding
          * may be activated for the next output, which we don't want,
          * so we postpone the switch */
@@ -86,6 +127,34 @@ class wayfire_oswitch : public wf::plugin_interface_t
         return true;
     };
 
+    wf::activator_callback switch_left = [=] (auto)
+    {
+        auto target_output = get_left_output();
+        switch_to_output(target_output);
+        return true;
+    };
+    
+    wf::activator_callback switch_right = [=] (auto)
+    {
+        auto target_output = get_right_output();
+        switch_to_output(target_output);
+        return true;
+    };
+    
+    wf::activator_callback switch_up = [=] (auto)
+    {
+        auto target_output = get_up_output();
+        switch_to_output(target_output);
+        return true;
+    };
+    
+    wf::activator_callback switch_down = [=] (auto)
+    {
+        auto target_output = get_down_output();
+        switch_to_output(target_output);
+        return true;
+    };
+
   public:
     void init()
     {
@@ -98,6 +167,10 @@ class wayfire_oswitch : public wf::plugin_interface_t
             &prev_output);
         bindings->add_activator(wf::option_wrapper_t<wf::activatorbinding_t>{"oswitch/prev_output_with_win"},
             &prev_output_with_window);
+        bindings->add_activator(wf::option_wrapper_t<wf::activatorbinding_t>{"oswitch/left_output"}, &switch_left);
+        bindings->add_activator(wf::option_wrapper_t<wf::activatorbinding_t>{"oswitch/right_output"}, &switch_right);
+        bindings->add_activator(wf::option_wrapper_t<wf::activatorbinding_t>{"oswitch/up_output"}, &switch_up);
+        bindings->add_activator(wf::option_wrapper_t<wf::activatorbinding_t>{"oswitch/down_output"}, &switch_down);
     }
 
     void fini()
@@ -107,6 +180,10 @@ class wayfire_oswitch : public wf::plugin_interface_t
         bindings->rem_binding(&next_output_with_window);
         bindings->rem_binding(&prev_output);
         bindings->rem_binding(&prev_output_with_window);
+        bindings->rem_binding(&switch_left);
+        bindings->rem_binding(&switch_right);
+        bindings->rem_binding(&switch_up);
+        bindings->rem_binding(&switch_down);
         idle_switch_output.disconnect();
     }
 };
