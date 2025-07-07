a08ab32992dc99bf2f5d08c183eab8ff9737fcda
killown <systemofdown@gmai.com>
2025-07-06 22:53:56 -0300

    improved the log message

--- plugins/ipc-rules/ipc-utility-methods.hpp
+++ plugins/ipc-rules/ipc-utility-methods.hpp
@@ -281,7 +281,10 @@ class ipc_rules_utility_methods_t
             return wf::ipc::json_error("Options must be an object!");
         }
 
-
+        const char *config_file = std::getenv("WAYFIRE_CONFIG_FILE");
+        std::string config_file_str = config_file ? config_file : "wayfire.ini";
+  
+        LOGW("Config options set via IPC will not update from ", config_file_str, " until Wayfire restart.");
 
         for (auto& option : data.get_member_names())
         {
@@ -306,13 +309,6 @@ class ipc_rules_utility_methods_t
                         std::string(json_to_string(data[option])) + "!");
                 }
             }
-
-            const char *config_file = std::getenv("WAYFIRE_CONFIG_FILE");
-            std::string config_file_str = config_file ? config_file : "wayfire.ini";
-
-            LOGW("Config option '", option, "' modified via IPC. ",
-                "Changes in ", config_file_str, " will not affect this option ",
-                "until Wayfire is restarted.");
 
             opt->set_locked(true);
         }
